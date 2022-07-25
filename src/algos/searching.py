f9d5023dd339a7595eed3f3306e8bc1feb4c2fa5
LuisGMM <emaildemiscuentas@gmail.com>
2022-07-25 14:51:32 +0200

    Update searching.py

--- src/algos/searching.py
+++ src/algos/searching.py
@@ -5,3 +5,22 @@ Real = Union[int, float]
 
 
 def linear_search(lst: List[Real], v: Real) -> Optional[int]:
+    """Searches for an element `v` in a list `lst`, returning its index if found.
+
+    See Also
+    ----------
+    Introduction to Algorithm page 5.
+
+    Parameters
+    ----------
+    lst : List[Real]
+        list to search in.
+    v : Real
+        element to be searched for in the list.
+
+    Returns
+    -------
+    Optional[int]
+        Index of the element found in the list.
+    """
+
